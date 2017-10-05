a8df774db5f28840b02b05629ccb682bc6e15555
Caleb <caleb@ramseymediaworks.com>
2017-10-05 13:36:37 -0500

    first push

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php
+	//SETS PAGE INFO AT THE TOP OF THE HEADER
+	$pageTitle = "About";
+	$pageName = "about";
+
+	include('assets/includes/header.php');
+?>
+<header>
+
+<div class="parent">
+<div class="full_Width container center jcc nav_Container">
+  <div class="w_1200 nav">
+  <div class="logo"><img src="/assets/images/dist/logo-gold.png"></div>
+    <div class="button_Container">
+    <div class="button mx-s p-s">Gallery</div>
+    <div class="button mx-s p-s">About</div>
+    <div class="button mx-s p-s">Contact</div>
+  </div>
+  </div>
+</div>
+<div class="main_Container full_Width column container jcc aic p-l">
+  <div class="w_1200">
+    <div class="container  primary-bg column aic jcc flex ">
+			<div class="logo"><img src="/assets/images/dist/logo-gold.png"></div>
+			<div class="about">al;sdkjf;laksjdf;lkasjasdfasfsdadsfasdfasdfasdf</div>
+    </div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+	//SETS PAGE INFO AT THE TOP OF THE HEADER
+	$pageTitle = "Home";
+	$pageName = "home";
+
+	include('assets/includes/header.php');
+?>
+<header>
+
+<div class="parent">
+<div class="full_Width container center jcc nav_Container">
+  <div class="w_1200 nav">
+  <img class="logo" src="/assets/images/dist/logo-gold.png">
+    <div class="button_Container">
+    <div class="button mx-s p-s">Gallery</div>
+    <div class="button mx-s p-s">About</div>
+    <div class="button mx-s p-s">Contact</div>
+  </div>
+  </div>
+</div>
+<div class=" full_Width container jcc p-l">
+  <div class="w_1200">
+    <div class="gallery m-l pt-xl">
+        <div class="gallery_Item m-l jcc aic">
+            <div class="title">Marvin Keller</div>
+        </div>
+          <div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Two</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Three</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Four</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Five</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+          <div class="title">Project Six</div>
+      	</div>
+				<div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Seven</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Eight</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+            <div class="title">Project Nine</div>
+        </div>
+        <div class="gallery_Item m-l jcc aic">
+          <div class="title">Project Ten</div>
+      	</div>
+      </div>
+  </div>
+</div>
+</div>
